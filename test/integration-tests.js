12e73828647d0be52d805a6abdf9362053683685
Vitaly Domnikov <dotcypress@gmail.com>
2017-12-10 15:15:56 -0800

    callback query

--- test/integration-tests.js
+++ test/integration-tests.js
@@ -13,16 +13,24 @@ test('simple bot', async (t) => {
   const bot = server.createBot()
   const chat = user.startBot(bot)
 
-  const exampleBot = new Telegraf(bot.token, {
-    telegram: {
-      apiRoot: server.getApiEndpoint()
-    }
-  })
+  const apiRoot = server.getApiEndpoint()
+  const exampleBot = new Telegraf(bot.token, { telegram: { apiRoot } })
   exampleBot.start(({ reply }) => reply('Ola'))
+  exampleBot.action('cat', async ({ answerCbQuery, reply }) => {
+    await answerCbQuery('🐈')
+    await reply('Meow')
+  })
   exampleBot.startPolling()
 
-  await wait(100)
-
+  await wait()
   t.is(chat.history.length, 2)
   t.is(chat.history[1].message.text, 'Ola')
+
+  chat.postCbQuery(user, chat.history[1].message, 'cat')
+
+  await wait()
+  t.is(chat.history.length, 5)
+  t.truthy(chat.history[3].callback_query_answer)
+  t.is(chat.history[3].callback_query_answer.text, '🐈')
+  t.is(chat.history[4].message.text, 'Meow')
 })
